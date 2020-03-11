6603146b792af7c3e14fb8988e431d6391d6ee8a
Samuel Burchfield <sam@Samuels-MBP.attlocal.net>
2020-03-10 21:22:15 -0400

    Updates

---
+++ precache-manifest.c8974726fc3eda3bb8aa31a34bc067b9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9e36be1c304b8e5501e330a14ccced",
+    "url": "/index.html"
+  },
+  {
+    "revision": "395fa7f568d1589e656c",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a50e634079caa307a376",
+    "url": "/static/css/main.d71d57e4.chunk.css"
+  },
+  {
+    "revision": "395fa7f568d1589e656c",
+    "url": "/static/js/2.25096fde.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.25096fde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50e634079caa307a376",
+    "url": "/static/js/main.53bf4779.chunk.js"
+  },
+  {
+    "revision": "86bb14a54e4bfaf5a83a",
+    "url": "/static/js/runtime-main.deba20db.js"
+  },
+  {
+    "revision": "a785ca19e35160514c3a978e5b91aad1",
+    "url": "/static/media/background.a785ca19.jpg"
+  },
+  {
+    "revision": "ef0e095d95ee7dae299c8c73abfe56b4",
+    "url": "/static/media/background0.ef0e095d.jpg"
+  },
+  {
+    "revision": "28bc066b17dd9e1418ef8b136f5426e1",
+    "url": "/static/media/fireworks.28bc066b.jpg"
+  },
+  {
+    "revision": "d1fa31aa01312f59e7781a7aa4d1687a",
+    "url": "/static/media/great-smokey-mountains.d1fa31aa.jpg"
+  },
+  {
+    "revision": "1d26a62de278793dc10500250ec47242",
+    "url": "/static/media/melton-lake.1d26a62d.jpg"
+  },
+  {
+    "revision": "9d6741e4f9168467304925a19367b87b",
+    "url": "/static/media/monument.9d6741e4.jpg"
+  },
+  {
+    "revision": "ea1efd5c9ae281c1a40d067d619cd212",
+    "url": "/static/media/tower.ea1efd5c.jpg"
+  },
+  {
+    "revision": "9886f996966d8429f21bef4f4648e97f",
+    "url": "/static/media/us-capitol-building.9886f996.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chance-for-congress/precache-manifest.e6882bf621eee684db7324efa54e3d87.js"
+  "/precache-manifest.c8974726fc3eda3bb8aa31a34bc067b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chance-for-congress/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
